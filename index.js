d7591a56119afc60cbbc8f6b42cbe74661fdb5da
Josue Reyes Cabral <70187068+Pablo-design25@users.noreply.github.com>
2022-02-21 22:16:40 -0600

    Add files via upload
    Primera y segunda vista en Ionic

---
+++ index.js
@@ -0,0 +1,17 @@
+console.long("hola, mundo xd");
+var total=0;
+//var monto =0;
+var boton = document.getElementById("calcularBtn");
+//var montoInput = document.getElementById("montoInput");
+
+var monto = montoInput.innerText;
+
+
+function calcularMonto(){
+//total = total + monto;
+console.log("Hiciste click en el boton")
+}
+
+function mensajeClick(){
+   console.log("Hiciste click en el boton");
+}
